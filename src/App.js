e7ac09f3f93b2c7045eb3bbb6e7188e59de4e280
Diego Acosta <diegoacosta211@gmail.com>
2018-08-29 12:51:11 -0300

    change app title style

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
+        <h1 style={{...defaultStyle, fontSize: '54px'}} class="App-title">Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter />
